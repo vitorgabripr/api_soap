68cd977c755cd35cd1426ebe91e43f4e6994af2b
Vitor <vitor.gabrielll0245@gmail.com>
2025-04-01 19:48:05 -0300

    Primeiro commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const soap = require('soap')
+const xml2js = require('xml2js')
+
+const app = express()
+const PORTA = 5000
+
+// continuar, introduzir a api soap e melhorar codigo
